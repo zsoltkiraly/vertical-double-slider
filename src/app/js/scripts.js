57b0ac6e8da4150f74aca1c97fa53fe85060d47b
zsoltkiraly <hello@zsoltkiraly.com>
2019-03-13 10:45:16 +0100

    Vertical double slider patch-004 { add signatura }

--- src/app/js/scripts.js
+++ src/app/js/scripts.js
@@ -29,6 +29,28 @@ if (hasTouch()) {
 
 var verticalDoubleSlider = function() {
 
+    function signatura() {
+        if (window['console']) {
+            const text = {
+                black: '%c     ',
+                blue: '%c   ',
+                author: '%c  Zsolt Király  ',
+                github: '%c  https://zsoltkiraly.com/'
+            }
+
+            const style = {
+                black: 'background: #282c34',
+                blue: 'background: #61dafb',
+                author: 'background: black; color: white',
+                github: ''
+            }
+
+            console.log(text.black + text.blue + text.author + text.github, style.black, style.blue, style.author, style.github);
+        }
+    }
+
+    signatura();
+
     function onTransition(el) {
         el.classList.remove('notransition');
     }
