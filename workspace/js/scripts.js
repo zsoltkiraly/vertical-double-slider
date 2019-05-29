6061711219b273bed4caca6c227c4fe998eba8a2
Zsolt Király <hello@zsoltkiraly.com>
2019-05-29 11:03:57 +0200

    vertical-double-slider patch-005 { switch from grunt to gulp }

--- workspace/js/scripts.js
+++ workspace/js/scripts.js
@@ -1,32 +1,9 @@
-'use strict';
-
 /*
 Vertical double slider - Code by Zsolt Király
 v1.0.2 - 2018-08-30
 */
 
-function hasTouch() {
-    return 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-}
-
-if (hasTouch()) {
-    try {
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
-
-
+'use strict';
 var verticalDoubleSlider = function() {
 
     function signatura() {
@@ -50,6 +27,27 @@ var verticalDoubleSlider = function() {
     }
 
     signatura();
+
+    function hasTouch() {
+        return 'ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
+    }
+    
+    if (hasTouch()) {
+        try {
+            for (var si in document.styleSheets) {
+                var styleSheet = document.styleSheets[si];
+                if (!styleSheet.rules) continue;
+    
+                for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+                    if (!styleSheet.rules[ri].selectorText) continue;
+    
+                    if (styleSheet.rules[ri].selectorText.match(':hover')) {
+                        styleSheet.deleteRule(ri);
+                    }
+                }
+            }
+        } catch (ex) {}
+    }
 
     function onTransition(el) {
         el.classList.remove('notransition');
